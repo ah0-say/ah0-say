0decc76253176207eb40489b4528c06e6f40a24f
ah0-say <tladkdud06@gmail.com>
2026-02-14 23:00:17 +0900

    [Silver IV] Title: 제로, Time: 8 ms, Memory: 1384 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cc
+++ 백준/Silver/10773. 제로/제로.cc
@@ -3,28 +3,29 @@
 
 int main()
 {
-    int N, a;
-    scanf("%d", &N);
+	int N, a;
+	int ans[100000];
+	int fin = 0;
 
-    int ans[100000];
-    int top = 0;
+	scanf("%d", &N);
 
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &a);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &a);
 
-        if (a == 0) {
-            if (top > 0) top--;
-        }
-        else {
-            ans[top++] = a;
-        }
-    }
+		if (a == 0) {
+			if (fin > 0) fin--;
+		}
+		else {
+			ans[fin] = a;
+			fin++;
+		}
+	}
+	int all = 0;
+	for (int i = 0; i < fin; i++) {
+		all += ans[i];
+	}
 
-    int all = 0;
-    for (int i = 0; i < top; i++) {
-        all += ans[i];
-    }
 
-    printf("%d\n", all);
-    return 0;
+	printf("%d\n", all);
+	return 0;
 }
